9747631675d349ed8026c45abb73c5882f2692a4
tushantac2003-prog <tushantac2003@gmail.com>
2026-05-17 09:11:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,11 +13,15 @@ st.write("Enter Applicant Details")
 income = st.number_input("ApplicantIncome")
 loan_amount = st.number_input("LoanAmount")
 cibil_score = st.number_input("Cibil_Score")
-dependents = st.number_input("Dependents")
+dependents = st.selectbox("No of Dependents", ["2","3+"])
+credit_history = st.selectbox("Previous_Loan_Taken",["Yes","No"])
+property_area = st.selectbox("Property_Area", ["Urban","Rural","Semiurban"])
+age = st.number_input("Age")
+married = st.selectbox("Married", ["Yes","No"])
 
 if st.button("Predict Loan Status"):
 
-    features = np.array([[income, loan_amount, cibil_score, dependents]])
+    features = np.array([[income, loan_amount, cibil_score, dependents, credit_history, age, property_area, married]])
 
     prediction = model.predict(features)
 
