b43f13207a7b7926257458f9cccc5e5cf432f333
tushantac2003-prog <tushantac2003@gmail.com>
2026-05-17 09:26:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ if st.button("Predict Loan Status"):
     credit_history = 1 if credit_history == "Yes" else 0
     married = 1 if married=="Yes" else 0
     property_area = {"Urban":0,"Rural":1,"Semiurban":2}
-    property_area = property_area_map(property_area)
+    property_area = property_area_map[property_area]
 
     features = np.array([[income, loan_amount, cibil_score, dependents, credit_history, age, property_area, married]])
 
